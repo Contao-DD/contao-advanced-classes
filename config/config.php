855e531fa189f060a47e8d2addafe9783903cd47
Mathias Arzberger <develop@pdir.de>
2017-02-13 15:47:41 +0100

    fix subfolder support

--- config/config.php
+++ config/config.php
@@ -23,14 +23,14 @@ if (TL_MODE == 'BE' & !$GLOBALS['TL_JAVASCRIPT']['jquery']) {
     } else {
         $GLOBALS['TL_JAVASCRIPT']['jquery'] = 'assets/jquery/core/' . reset((scandir(TL_ROOT . '/assets/jquery/core', 1))) . '/jquery.min.js';
     }
-    $GLOBALS['TL_JAVASCRIPT']['noconflict'] = TL_ROOT .'/system/modules/advanced_classes/assets/js/jquery.noconflict.js';
-    $GLOBALS['TL_JAVASCRIPT']['advanced_classes'] = TL_ROOT .'/system/modules/advanced_classes/assets/js/jquery.advanced_classes.js';
+    $GLOBALS['TL_JAVASCRIPT']['noconflict'] = 'system/modules/advanced_classes/assets/js/jquery.noconflict.js';
+    $GLOBALS['TL_JAVASCRIPT']['advanced_classes'] = 'system/modules/advanced_classes/assets/js/jquery.advanced_classes.js';
 }
 
 /**
  * Css
  */
 if (TL_MODE == 'BE') {
-    $GLOBALS['TL_CSS']['advanced_classes'] = TL_ROOT .'/system/modules/advanced_classes/assets/css/advanced_classes.css';
-    $GLOBALS['TL_CSS']['font-awesome'] = TL_ROOT .'/system/modules/advanced_classes/assets/vendor/fontello/css/icon.css';
+    $GLOBALS['TL_CSS']['advanced_classes'] = 'system/modules/advanced_classes/assets/css/advanced_classes.css';
+    $GLOBALS['TL_CSS']['font-awesome'] = 'system/modules/advanced_classes/assets/vendor/fontello/css/icon.css';
 }
